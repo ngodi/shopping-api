d15ede07a3ea4764167ca7f30299acb15965e6aa
ngodi <albertngodi@gmail.com>
2021-11-08 20:41:10 +0100

    adds product serializer for custom json output

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
-            render json: @user, status: :created
+            render json: UserSerializer.new(@user).serializable_hash, status: :created
         else
             render json: @user.errors, status: :unprocessable_entity
         end
@@ -18,7 +18,7 @@ class Api::V1::UsersController < ApplicationController
 
     def update
         if @user.update(user_params)
-             render json: @user, status: :ok
+             render json: UserSerializer.new(@user).serializable_hash, status: :ok
         else
              render json: @user.errors, status: :unprocessable_entity
         end
