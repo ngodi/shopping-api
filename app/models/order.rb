933532254e303c509e9db07c134be7e3f39d0ace
ngodi <albertngodi@gmail.com>
2021-11-11 06:43:32 +0100

    updates the total calculation for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
+  include EnoughProductsValidator
+
   before_validation :set_total!
 
   belongs_to :user
@@ -7,9 +9,13 @@ class Order < ApplicationRecord
   
   validates :total, numericality: { greater_than_or_equal_to: 0 }
   validates :total, presence: true
+  validates_with EnoughProductsValidator
 
   def set_total!
-    self.total = products.map(&:price).sum
+    def set_total!
+      self.total = self.placements
+      .map{ |placement| placement.product.price * placement.quantity }.sum
+      end
   end
 
   def build_placements_with_product_ids_and_quantities(product_ids_and_quantities)
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -8,12 +8,15 @@ class OrderTest < ActiveSupport::TestCase
     @product2 = products(:two)
   end
 
-  test 'Should set total' do
-    order = Order.new user_id: @order.user_id
-    order.products << products(:one)
-    order.products << products(:two)
-    order.save
-    assert_equal (@product1.price + @product2.price), order.total
+  test "Should set total" do
+    @order.placements = [
+      Placement.new(product_id: @product1.id, quantity: 2),
+      Placement.new(product_id: @product2.id, quantity: 2)
+    ]
+    @order.set_total!
+    expected_total = (@product1.price * 2) + (@product2.price *
+    2)
+    assert_equal expected_total, @order.total
   end
 
   test 'builds 2 placements for the order' do
@@ -24,6 +27,11 @@ class OrderTest < ActiveSupport::TestCase
     assert_difference('Placement.count', 2) do
        @order.save
     end
+  end
+
+  test "an order should command not too much product than available" do
+    @order.placements << Placement.new(product_id: @product1.id, quantity: (1 + @product1.quantity))
+    assert_not @order.valid?
   end
 
 end
